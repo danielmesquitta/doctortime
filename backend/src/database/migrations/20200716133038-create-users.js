433567f10360c8d1b63837bc32ed234f3feacae2
Daniel Mesquita <danielmesquitta123@gmail.com>
2020-07-16 10:37:22 -0300

    user migration

---
+++ backend/src/database/migrations/20200716133038-create-users.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  down: async queryInterface => {
+    await queryInterface.dropTable('users')
+  },
+}
