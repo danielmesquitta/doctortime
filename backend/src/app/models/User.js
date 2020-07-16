c979b0577906ce906bc549c7bb3d86152bf5b033
Daniel Mesquita <danielmesquitta123@gmail.com>
2020-07-16 12:00:37 -0300

    added bcryptjs to generate password hash

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const { Model } = Sequelize
+const bcrypt = require('bcryptjs')
 
 class User extends Model {
   static init(sequelize) {
@@ -7,6 +8,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -14,6 +16,12 @@ class User extends Model {
         sequelize,
       }
     )
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8)
+      }
+    })
   }
 }
 
