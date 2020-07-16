b1eee6542f7873a636f412c568bb6b44aa9210f6
danielmesquitta <danielmesquitta123@gmail.com>
2020-07-16 20:59:03 -0300

    added appointment cancelation

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,5 +1,11 @@
 const Yup = require('yup')
-const { startOfHour, parseISO, isBefore, format } = require('date-fns')
+const {
+  startOfHour,
+  parseISO,
+  isBefore,
+  format,
+  subHours,
+} = require('date-fns')
 const { pt } = require('date-fns/locale')
 
 const User = require('../models/User')
@@ -89,6 +95,27 @@ class AppointmentController {
     })
 
     return res.json(appointment)
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id)
+
+    if (appointment.user_id !== req.userId) {
+      res
+        .status(401)
+        .json({ error: "You don't have permission to cancel this appointment" })
+    }
+
+    const latestDateToCancel = subHours(appointment.date, 2)
+    if (isBefore(latestDateToCancel, new Date())) {
+      return res
+        .status(400)
+        .json({ error: 'You can only cancel appointments 2 hours in advance' })
+    }
+    appointment.canceled_at = new Date()
+    await appointment.save()
+
+    res.json(appointment)
   }
 }
 
