d2e84ed8154e670b1d6c8e621c0c50c835f88c36
danielmesquitta <danielmesquitta123@gmail.com>
2020-07-16 15:25:47 -0300

    data validation

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -1,10 +1,19 @@
 const jwt = require('jsonwebtoken')
+const Yup = require('yup')
 
 const User = require('../models/User')
 const authConfig = require('../../config/auth')
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    })
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' })
+    }
+
     const { email, password } = req.body
 
     const user = await User.findOne({ where: { email } })
--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,7 +1,18 @@
+const Yup = require('yup')
+
 const User = require('../models/User')
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    })
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' })
+    }
+
     const userExists = await User.findOne({
       where: { email: req.body.email },
     })
@@ -13,6 +24,23 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string(),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    })
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' })
+    }
+
     const { email, oldPassword } = req.body
 
     const user = await User.findByPk(req.userId)
