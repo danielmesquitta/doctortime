6bfe86a95b9be1e112b9b6621a3bec42a17d219a
Daniel Mesquita <danielmesquitta123@gmail.com>
2020-07-21 19:36:27 -0300

    added and configured redux, reactotron and axios

--- mobile/android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ mobile/android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,6 +11,8 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+// @react-native-community/async-storage
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 // @react-native-community/masked-view
 import org.reactnative.maskedview.RNCMaskedViewPackage;
 // react-native-gesture-handler
@@ -70,6 +72,7 @@ public class PackageList {
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
+      new AsyncStoragePackage(),
       new RNCMaskedViewPackage(),
       new RNGestureHandlerPackage(),
       new LinearGradientPackage(),
--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,17 +1,22 @@
 import 'react-native-gesture-handler'
-
 import React from 'react'
 import { StatusBar } from 'react-native'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux'
+import './config/reactotron'
 
 import Routes from './routes'
+import { store, persistor } from '~/store'
 import { colors } from './styles/variables'
 
 function App() {
   return (
-    <>
-      <StatusBar backgroundColor={colors.mainDark} barStyle="light-content" />
-      <Routes />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar backgroundColor={colors.mainDark} barStyle="light-content" />
+        <Routes />
+      </PersistGate>
+    </Provider>
   )
 }
 
